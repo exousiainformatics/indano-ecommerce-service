948b6557a7083e676298db042df1e1cec598b787
RajeevKumarSinha <rajeev.exousia@gmail.com>
2021-02-28 23:25:21 +0530

    adding ajio,tataclicq,amazonglobal,contact

--- src/views/career/carrier/carrier.js
+++ src/views/career/carrier/carrier.js
@@ -1,28 +1,28 @@
-import React, { Component,useState,props } from 'react'
-import { Container, Image, Row, Col, Form, Button,Modal, } from 'react-bootstrap';
-import Banner from '../../../assets/images/home/career-img/banner-c.png';
-import Schedule from '../../../assets/images/home/career-img/Vector.png';
-import Mother from '../../../assets/images/home/career-img/mother.png';
-import Shirt from '../../../assets/images/home/career-img/shirt.png';
-import Flag from '../../../assets/images/home/career-img/flag.png';
-import Network from '../../../assets/images/home/career-img/network.png';
-import Training from '../../../assets/images/home/career-img/training.png';
-import Calendar from '../../../assets/images/home/career-img/calendar.png';
+import React, { Component, useState, props } from 'react'
+import { Container, Image, Row, Col, Form, Button, Modal, } from 'react-bootstrap';
+import Banner from '../../../assets/images/career-img/banner-c.png';
+import Schedule from '../../../assets/images/career-img/Vector.png';
+import Mother from '../../../assets/images/career-img/mother.png';
+import Shirt from '../../../assets/images/career-img/shirt.png';
+import Flag from '../../../assets/images/career-img/flag.png';
+import Network from '../../../assets/images/career-img/network.png';
+import Training from '../../../assets/images/career-img/training.png';
+import Calendar from '../../../assets/images/career-img/calendar.png';
 
- export default function Career () {
-// constructor(props){
-//     super(props);
-//     this.state = {
-//       isOpen: false,
-//       isClose:true
-//     };
-//   }
+export default function Career() {
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //       isOpen: false,
+    //       isClose:true
+    //     };
+    //   }
 
-  
-const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <Container fluid className="p-0">
             <div style={{ marginTop: '6%' }}>
@@ -33,47 +33,47 @@ const [show, setShow] = useState(false);
         Launch demo modal
       </Button> */}
 
-      <Modal show={show} onHide={handleClose}{...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      >
-        <Modal.Header className="body-color" closeButton>
-          <Modal.Title className="body-color w-100"><Row>
-                     <Col className="col-8 text-color pt-2"><h1 className="font-mobile-biggerrr">Experience Required</h1></Col>
-                     <Col className="col-4 text-color pt-2"><h1 className="font-mobile-biggerrr">2-3 Years</h1></Col>
-                 </Row></Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="body-color"><Form className="pb-2">
-                     <Row>
-                         <Col className="col-4 col-md-2">      <p className="text-color">Apply For :</p></Col>
-                         <Col className="col-8 col-md-10">   <select className="mb-2 text-secondary w-100">
-                             <option value="Ford">Content Writer</option>
-                             <option value="Volvo">Digital Marketing Expertise</option>
-                             <option value="Fiat">Web Designer</option>
-                         </select></Col>
-                     </Row>
-                     <Row>
-                         <Col md={6}>
-                             <Form.Control type='text' placeholder='Enter Your Name' className="homeName mb-2" />
-                         </Col>
-                         <Col md={6}>
-                             <Form.Control type='email' placeholder='Email Id' />
-                         </Col>
-                     </Row>
-                     <Row className="mt-md-3 mt-2">
-                         <Col md={10}>
-                             <Form.Control type='number' placeholder="Phone No" />
-                         </Col>
-                         <Col md={2} className="mt-2 mt-md-0">
-                             <Button type="submit" className="homeFormBtnn">
+            <Modal show={show} onHide={handleClose}{...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header className="body-color" closeButton>
+                    <Modal.Title className="body-color w-100"><Row>
+                        <Col className="col-8 text-color pt-2"><h1 className="font-mobile-biggerrr">Experience Required</h1></Col>
+                        <Col className="col-4 text-color pt-2"><h1 className="font-mobile-biggerrr">2-3 Years</h1></Col>
+                    </Row></Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="body-color"><Form className="pb-2">
+                    <Row>
+                        <Col className="col-4 col-md-2">      <p className="text-color">Apply For :</p></Col>
+                        <Col className="col-8 col-md-10">   <select className="mb-2 text-secondary w-100">
+                            <option value="Ford">Content Writer</option>
+                            <option value="Volvo">Digital Marketing Expertise</option>
+                            <option value="Fiat">Web Designer</option>
+                        </select></Col>
+                    </Row>
+                    <Row>
+                        <Col md={6}>
+                            <Form.Control type='text' placeholder='Enter Your Name' className="homeName mb-2" />
+                        </Col>
+                        <Col md={6}>
+                            <Form.Control type='email' placeholder='Email Id' />
+                        </Col>
+                    </Row>
+                    <Row className="mt-md-3 mt-2">
+                        <Col md={10}>
+                            <Form.Control type='number' placeholder="Phone No" />
+                        </Col>
+                        <Col md={2} className="mt-2 mt-md-0">
+                            <Button type="submit" className="homeFormBtnn">
                                 Submit
                                         </Button>
-                         </Col>
-                     </Row>
-          </Form></Modal.Body>
-      </Modal>
-{/* modal section over */}
+                        </Col>
+                    </Row>
+                </Form></Modal.Body>
+            </Modal>
+            {/* modal section over */}
             <div className="text-center">
                 <h1 className="mt-5 font-weight-bold">Equal opportunities & Equal Chances</h1>
                 <h1 className="font-weight-bold">Welcome To The World Of</h1>
@@ -89,17 +89,17 @@ const [show, setShow] = useState(false);
                 {/* welcome section over */}
                 <h1 className="e-text font-weight-bold mb-5">Open Positions</h1>
                 <div className="row col-md-9 mx-auto p-0 mb-5 pos-container">
-                
-                    <div className="position-card col-11 col-md-3 shadow mx-auto" onClick={handleShow}>
+
+                    <div className="position-card col-11 col-md-3 shadow mx-auto cursor" onClick={handleShow}>
                         <h5 className="p-3 pt-5">Content Writer</h5>
                         <p className="detail">View Details</p>
                     </div>
-                
-                    <div className="position-card col-11 col-md-3 shadow mx-auto" onClick={handleShow}>
+
+                    <div className="position-card col-11 col-md-3 shadow mx-auto cursor" onClick={handleShow}>
                         <h5 className="p-3 pt-5">Digital Marketing Expertise</h5>
                         <p className="detail">View Details</p>
                     </div>
-                    <div className="position-card col-11 col-md-3 shadow mx-auto" onClick={handleShow}>
+                    <div className="position-card col-11 col-md-3 shadow mx-auto cursor" onClick={handleShow}>
                         <h5 className="pb-3 pt-5">Web Designer</h5>
                         <p className="detail">View Details</p>
                     </div>
--- src/views/tataCliq/mobileView/index.js
+++ src/views/tataCliq/mobileView/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import Group from '../../../assets/images/tatacliq-img/Group.png'
 
 export default function index() {
     return (
-        <div>
-            hello
-        </div>
+        <Container style={{ marginTop: '30%' }}>
+            <h1 className="serviceHeadText font-mobile-extra-bigger text-center">TataCliq</h1>
+            <h5 className="mt-3 text-muted font-italic text-justify font-weight-bold">Its one of leading e-commerce platform around indian marketplace. Tatacliq give their sellers appropriate chance to get onboard for online selling</h5>
+            <Row className="mt-5">
+                <Col className="col-12">
+                    <h3 className="text-orange font-weight-bold mb-3">Seller's Training</h3>
+                    <Container className="col-10 mb-4"><Image src={Group} width={'100%'} className="pt-3 mt-sm-0 pt-sm-0"/></Container>
+                    <p className="font-weight-bold">Get training on everything, how selling on Tataclip works, day to day operations, how to check your orders, how to manage your Tatacliq account and so on.</p>
+                    <p className="font-weight-bold">There are few documents needed for seller onboard process:</p>
+                    <ul className="font-weight-bold">
+                        <li>PAN card</li>
+                        <li>GST certificate</li>
+                        <li>CIN</li>
+                        <li>Cancelled cheque</li>
+                        <li>Company logo</li>
+                        <li>Bank confirmation on electronic clearence form with attachment.</li>
+                    </ul>
+                </Col>
+            </Row>
+            <h2 className=" mt-3 text-center serviceHeadText font-mobile-biggerr">Benefits of selling with Tatacliq</h2>
+            <Row className="mt-4 mb-5">
+                <Col className="col-6 font-weight-bold"><p>Plethora of customers</p>
+                    <p>Valuable tips</p></Col>
+                <Col className="col-6 font-weight-bold"><p>Easily monitor your sales</p>
+                    <p>Transparency</p></Col>
+            </Row>
+        </Container>
     )
 }
