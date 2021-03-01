6be1a3ba6484454eb83146db334356d849b66006
vishwapathak <vishwa.exousia@gmail.com>
2021-03-01 13:08:16 +0530

    Updates

--- src/views/myntra/discpView/mobileView/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BottomUpScroll from '../../../../components/scrollComponent/bottomUpScroll';
-export default function index(props) {
-    return (
-        <div>
-            <BottomUpScroll data={props.data} />
-        </div>
-    )
-}
